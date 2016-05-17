c7a5e7ea2b1d6e5ce4832dd7d150dc5ad18adcae
yjilili <Jilili@Yiredas-MacBook-Pro.local>
2016-05-17 14:44:52 -0400

    lots going 5.17

--- bashChat.sh
+++ bashChat.sh
@@ -7,8 +7,11 @@ then
 	exit 1
 elif [ $# -lt 2 ]
 then
+	room=$1
+	./checkname.sh $1 "room"
+	cd $1
 	read -p "What is your nick name?" name
-	./checkname.sh $name "user"
+	../checkname.sh $name "user"
 	inClass="0"
 else
 	echo "Please enter one chat room name"
@@ -19,12 +22,20 @@ while [ $inClass -eq "0" ]
 do
 	STR=$'Please choose option:\n\n(1)Exit\n(2)See Users\n(3)Enter Text\n'
 	read -p "$STR" option
-	if [ $option -eq "0" ]
+	if [ $option -eq "2" ]
 	then
-		echo "~~$name entered chat room $1~"
+		cat user.txt
+		echo "~~$name entered chat room $1~~"	
+	elif [ $option -eq "1" ]
+	then
+		echo "exiting chat room"
 		inClass=1
+	elif [ $option -eq "3" ]
+	then
+		read -p "Enter text: " entry
+		echo "$name : $entry"
+		echo "$name : $entry" >> room.txt
 	else
-		echo "here"
-		inClass=1
+		echo "please enter option"
 	fi
 done
--- checkname.sh
+++ checkname.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 current=$1
-names="currentNames.txt"
-
-userMatch() {
-	chmod 777 $names
-	grep -q "$current" $names || echo $current >> $names  echo "Username is already in use in this chatroom. Please choose another username."
-	return 0;	
-}
 
 roomMatch(){
-	file="${current}.txt"
+	file="${current}"
 	if [ ! -e file ]
 	then
-		echo "touch $file"
-		echo "created file: $file"
-	fi 
+		mkdir $file
+		touch ./$file/room.txt
+		touch ./$file/user.txt
+		cd $file
+		echo "created and moved into file: $file"
+	elif [ -e file ]
+	then
+		echo "file exists!!! moving into $file"
+		cd $file
+	else
+		echo "error"
+	fi
 }
 
-if [ $2 = "name" ]
+userMatch() {
+	chmod 777 user.txt
+	if [ -e "user.txt" ]
+	then 
+		echo "user file exists!"
+		echo "$1" >> user.txt
+		#grep -q "$current" $names || $current >> $names  echo "Username is already in use in this chatroom. Please choose another username."
+	elif [ ! -e file ]
+	then
+		echo "user file does not exists"
+		echo "currently in file $PWD"	
+	else
+		echo "error"
+	fi
+}
+
+
+if [ $2 == 'room' ]
 then
-	userMatch $1
-elif [ $2 = "room" ]
-then	
 	roomMatch $1
+elif [ $2 == 'user' ]
+then	
+	userMatch $1
+else echo "error, input not name or room"
 fi
